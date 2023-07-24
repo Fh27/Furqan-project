2eab5c13def9e585dac6212b53684b0d51cc30c3
Mohammed Faizan Ahmed <faizanzaid789@gmail.com>
2023-07-24 22:08:31 +0530

    Bug fixes

--- controllers/faculty/editFaculty.js
+++ controllers/faculty/editFaculty.js
@@ -6,25 +6,20 @@ const Form = require("../../models/FormModel");
 const editFaculty = async (req, res) => {
   try {
     const { facultyId, formIds } = req.body;
+    if(formIds.length === 0) {
+      return res.status(400).json({ error: "Please select at least one form" });
+    }
 
     // pass formd ids as an array
 
     // Find the faculty by ID
     const faculty = await Faculty.findById(facultyId);
-
-    if (!faculty) {
-      return res.status(404).json({ error: "Faculty not found" });
-    }
-
-    // Find the forms by their IDs
-    const forms = await Form.find({ _id: { $in: formIds } });
-
-    if (!forms) {
-      return res.status(404).json({ error: "One or more forms not found" });
-    }
+    
+   
+ 
 
     // Add the forms to the faculty's forms array
-    faculty.forms.push(...formIds);
+    faculty.forms = formIds;
 
     // Save the updated faculty
     await faculty.save();
